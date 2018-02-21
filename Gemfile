97e4fabee06367dc83ec233ae3af8e8cb802428b
Nabin Paudyal <nabindenepali@outlook.com>
2018-02-21 16:35:40 +0545

    Add user model using devise

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'puma', '~> 3.7'
 #Api gem
 gem 'active_model_serializers'
 
+#User authentication gem devise
+gem 'devise', '~> 4.2'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 GantabyaApi::Application.routes.draw do
+  devise_for :users
   # Definition of Gantabya Rails Api
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
