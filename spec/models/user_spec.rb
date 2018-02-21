f00c1e30265a73319f53bd01a3f3e937616066fc
Nabin Paudyal <nabindenepali@outlook.com>
2018-02-21 17:50:32 +0545

    Add specs for the user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:user) { create(:user) }
+
+  it 'responds to email' do
+    expect(user).to respond_to(:email)
+  end
+
+  it 'responds to password' do
+    expect(user).to respond_to(:password)
+  end
+
+  it 'responds to be valid' do
+    expect(user).to be_valid
+  end
 end
