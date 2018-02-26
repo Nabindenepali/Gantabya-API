45365cb4e78522a43493e0004376ce4ad18e77b3
Nabin Paudyal <nabindenepali@outlook.com>
2018-02-26 16:03:35 +0545

    Add update action for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,12 +6,22 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: 201
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
     else
-      render json: {errors: @user.errors}, status: 422
+      render json: {errors: user.errors}, status: 422
     end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: {errors: user.errors}, status: 422
+    end
+
   end
 
   private
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,44 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
 
-  let(:user) { create(:user) }
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+  before {request.headers['Accept'] = "application/vnd.marketplace.v1"}
 
   describe "GET #show" do
     context "when valid user id is passed" do
-      before { get :show, params: {id: user.id}, format: :json }
+      let(:user) { create(:user) }
+      before { get :show, params: { id: user.id }, format: :json }
 
       it "returns the information about a reporter on a hash" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:email]).to eql user.email
       end
 
-      it { should respond_with 200 }
+      it {should respond_with 200}
     end
   end
 
   describe "POST #create" do
-
     context "when valid user parameters are passed" do
-      before do
-        @user_attributes = FactoryBot.attributes_for :user
-        post :create, params: {user: @user_attributes}, format: :json
-      end
+      let(:valid_user_attributes) { FactoryBot.attributes_for :user }
+      before { post :create, params: { user: valid_user_attributes }, format: :json }
 
       it "renders the json representation for the user record just created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql @user_attributes[:email]
+        expect(user_response[:email]).to eql valid_user_attributes[:email]
       end
 
-      it { should respond_with 201 }
+      it {should respond_with 201}
     end
 
     context "when invalid user parameters are passed" do
-      before do
-        @invalid_user_attributes = { password: "12345678",
-                                     password_confirmation: "12345678" }
-        post :create, params: {user: @invalid_user_attributes}, format: :json
-      end
+      let(:invalid_user_attributes) {{ password: "12345678",
+                                       password_confirmation: "12345678" }}
+      before { post :create, params: { user: invalid_user_attributes }, format: :json }
 
       it "renders an errors json" do
         user_response = JSON.parse(response.body, symbolize_names: true)
@@ -49,6 +44,36 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it "renders the json errors on why the user could not be created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it {should respond_with 422}
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    let(:user) { create(:user) }
+    context "when the user exists and valid user parameters are passed" do
+      before { put :update, params: { id: user.id, user: { email: 'newmail@example.com' } }, format: :json }
+
+      it "renders the json representation of updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql 'newmail@example.com'
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when the user exists but invalid email is passed" do
+      before { put :update, params: { id: user.id, user: { email: 'badmail.com' } }, format: :json }
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "returns an error specifying entered email was invalid" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include 'is invalid'
       end
 
       it { should respond_with 422 }
