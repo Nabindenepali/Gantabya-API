303766ebe129ee13613074091a8a3415a9466768
Nabin Paudyal <nabindenepali@outlook.com>
2018-02-27 11:30:39 +0545

    Add user authentication token mechanism

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :auth_token, allow_blank: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      new_token = Devise.friendly_token
+    end while User.exists?(auth_token: new_token)
+    self.auth_token = new_token
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-
-  let(:user) {create(:user)}
+  let(:user) { create(:user) }
 
   describe 'responds to methods' do
-
     context 'when the fields are valid' do
-
       it 'responds to email' do
         expect(user).to respond_to(:email)
       end
@@ -16,22 +13,40 @@ RSpec.describe User, :type => :model do
         expect(user).to respond_to(:password)
       end
 
-      it 'responds to be valid' do
+      it 'is valid' do
         expect(user).to be_valid
       end
 
+      it { should respond_to(:auth_token) }
     end
-
   end
 
   describe 'Validations for the user model' do
-
     context 'given the correct confirmation password' do
       it { should validate_confirmation_of(:password) }
     end
 
+    context 'given a unique auth token' do
+      it { should validate_uniqueness_of(:auth_token) }
+    end
   end
 
+  describe '#generate_authentication_token!' do
+    context 'when there is no previous token' do
+      it 'generates a unique token' do
+        user.generate_authentication_token!
+        expect(User.exists?(auth_token: user.auth_token)).to be_falsey
+      end
+    end
+
+    context 'when a token has already been taken' do
+      it 'generates a different token' do
+        existing_user = FactoryBot.create(:user, auth_token: 'auniquetoken123')
+        user.generate_authentication_token!
+        expect(user.auth_token).not_to eql existing_user.auth_token
+      end
+    end
+  end
 end
 
 
